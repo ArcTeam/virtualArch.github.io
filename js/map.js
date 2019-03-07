abad9ea27137901d8bf3581bcfc29ee4af51c9fd
Giuseppe Naponiello <beppenapo@arc-teamcom>
2019-03-07 10:53:21 +0100

    init project

---
+++ js/map.js
@@ -0,0 +1,4 @@
+// map = new L.Map('map').setView([51.505, -0.09], 13);
+// osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+// osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+// osm = new L.TileLayer(osmUrl, {minZoom: 5, attribution: osmAttrib}).addTo(map);
