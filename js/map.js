5aaeba15e99b3bd92eac0824697e775ee55ba36f
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-04-05 09:40:52 +0200

    prepare gui

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
+element = $('#main');
+element.height(element.height() - 42);
 map = new L.Map('map').setView([51.505, -0.09], 13);
-osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+osmUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 osm = new L.TileLayer(osmUrl, {minZoom: 5, attribution: osmAttrib}).addTo(map);
