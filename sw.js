719c6ef8a8ede4a50c608b287e6f9c6d76edbddc
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-05-05 23:31:51 +0200

    service worker

--- sw.js
+++ sw.js
@@ -15,14 +15,14 @@ self.addEventListener('install', function(e) {
 });
 
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || new Response("Nothing in the cache for this request");
-    })
-  );
-});
+//self.addEventListener('fetch', function(event) {
+//  event.respondWith(
+//    caches.match(event.request).then(function(response) {
+//      return response || new Response("Nothing in the cache for this request");
+//    })
+//  );
+//});
 
-// self.addEventListener('fetch', function(event) {
-//  console.log(event.request.url);
-// });
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+});
