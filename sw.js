5713152e360cca7048c1c09986f90e63752ce9fd
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-05-21 13:27:46 +0200

    update service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const version = "0.0.1";
 const cacheName = `virtualArch-${version}`;
-const cacheFile = ['/','/virtualArch.github.io/'];
+const cacheFile = ['/virtualArch.github.io/'];
 self.addEventListener('install', e => {
   const timeStamp = Date.now();
   e.waitUntil(
